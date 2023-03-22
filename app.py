f679d514641a87f9ab99718d2ec09acd73a823a7
saurabhmamode <108463806+saurabhmamode@users.noreply.github.com>
2023-03-22 16:29:39 +0530

    new

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+# import flask module
 from flask import Flask
-import configparser
-from flask import render_template
-
+ 
+# instance of flask application
 app = Flask(__name__)
-
+ 
+# home route that returns below text
+# when root url is accessed
 @app.route("/")
-def home():
-    return {}
-
-if __name__ == "__main__":
-  app.run()
+def hello_world():
+    return "<p>Hello, World!</p>"
+ 
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost', port=8080)
