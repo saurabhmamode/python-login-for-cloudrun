9db16030b0b791e7fa56dffafbbab9a63bb4be54
saurabhmamode <108463806+saurabhmamode@users.noreply.github.com>
2023-03-23 17:06:16 +0530

    Add files via upload

---
+++ app (1).py
@@ -0,0 +1,14 @@
+# import flask module
+from flask import Flask
+ 
+# instance of flask application
+app = Flask(__name__)
+ 
+# home route that returns below text
+# when root url is accessed
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+ 
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
