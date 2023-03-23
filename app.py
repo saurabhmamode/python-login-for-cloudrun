ce566668f9746dbf441616d2cbb18c65d34b3e68
saurabhmamode <108463806+saurabhmamode@users.noreply.github.com>
2023-03-23 17:24:37 +0530

    for trigger test

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # when root url is accessed
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World 2!</p>"
  
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
